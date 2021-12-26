e9bf5a1723d52d7ab607da32d514f0a495df050d
EranDTZ <tzarum77@gmail.com>
2021-12-26 20:37:34 +0200

    bugfix

--- src/Node.py
+++ src/Node.py
@@ -3,7 +3,7 @@ from GeoLocation import GeoLocation
 
 class Node:
 
-    def __init__(self,pos : {},id : int):
+    def __init__(self,id : int,pos: tuple = None):
         self.pos = pos
         self.id = id;
         self.tag = 0;
